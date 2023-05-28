59c6da768fb36e9c3ddbe60ed4720948dae052cf
Do Huu Trung <dohuutrung123456@gmail.com>
2023-05-29 00:12:50 +0700

    bai

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const app = express()
+const port = 3000
+var path = require('path');
+
+// template engine
+app.engine('hbs', handlebars.engine({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views',  path.join(__dirname, 'resources/views'));
+
+// morgan
+app.use(morgan('combined'))
+
+// static img
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
